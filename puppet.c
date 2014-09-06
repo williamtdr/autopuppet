02ef3926b5d22de23bbc634c447acb4c53a0b583
williamtdr <william@cboos.com>
2014-09-06 18:10:02 -0500

    Monitor Joysticks

---
+++ puppet.c
@@ -0,0 +1,53 @@
+#pragma config(Hubs,  S1, HTMotor,  none,     none,     none)
+#pragma config(Sensor, S1,     ,                    sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C3_1,     RightDrive,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     LeftDrive,     tmotorTetrix, openLoop)
+
+#include "JoystickDriver.c"
+
+///////////////////////////////
+//							 //
+// 		Puppet Settings 	 //
+//							 //
+///////////////////////////////
+
+// JoystickPrecision: The threshold for error on the joysticks.
+// This is the tolerance for the joysticks being considered 'idle' by the program
+// and therefore the program 'sleeping'. This is also used to determine whether
+// the robot is going forward or backwards.
+// Lower value = more precise.
+#define JoystickPrecision	5
+
+typedef struct {
+	int joy1_y1;
+	int joy1_y2;
+	int joy1_x1;
+	int joy1_x2;
+
+	int joy2_y1;
+	int joy2_y2;
+	int joy2_x1;
+	int joy2_x2;
+} joystickForm;
+
+joystickForm joystickState;
+
+bool stopped = false;
+task joystickMonitor() {
+	while (true) {
+		getJoystickSettings(joystick);
+
+		joystickState.joy1_y1 = joystick.joy1_y1;
+		joystickState.joy1_y2 = joystick.joy1_y2;
+		joystickState.joy2_x1 = joystick.joy1_x1;
+		joystickState.joy2_x2 = joystick.joy1_x2;
+	}
+}
+
+task main() {
+	StartTask(joystickMonitor);
+
+	while(stopped == false) {
+		Sleep(1);
+	}
+}
